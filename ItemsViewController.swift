a16d21a5e35584b6bc7cd93e3fe9a311637babfb
Mark Engen <markengen@marks-air>
2017-11-11 21:02:16 -0500

    Chapter 14 - Bronze Challenge complete (dial pad vs std keyboard), silver challenge ignored and before attempting the Gold Challenge (allow modification of the date created value) via a new button and view controller.

--- ItemsViewController.swift
+++ ItemsViewController.swift
@@ -26,22 +26,6 @@ class ItemsViewController: UITableViewController {
         
     }
     
-    // Edit button
-    @IBAction func toggleEditingMode(_ sender: UIButton) {
-        // If you are currently in editing mode...
-        if isEditing {
-            sender.setTitle("Edit", for: .normal)
-            
-            // Turn off editing mode
-            setEditing(false, animated: true)
-        } else {
-            // Change text of button to inform user of state
-            sender.setTitle("Done", for: .normal)
-            
-            // Enter editing mode
-            setEditing(true, animated:true)
-        }
-    }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         print(" Starting override func tableView-A")
@@ -51,16 +35,6 @@ class ItemsViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         print(" Started viewDidLoad() from ItemsViewController.swift")
-        // Get the height of the status bar
-        let statusBarHeight = UIApplication.shared.statusBarFrame.height
-        
-        let insets = UIEdgeInsets(top: statusBarHeight, left: 0, bottom: 0, right: 0)
-        
-        //Table to start under statusBar
-        tableView.contentInset = insets
-        
-        //Scroll indicators to start under statusBar as well
-        tableView.scrollIndicatorInsets = insets
         
         //tableView.rowHeight = 65
         tableView.rowHeight = UITableViewAutomaticDimension
